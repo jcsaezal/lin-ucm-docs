49b3bb40b6f47d9be3679e9e58d15d095b576060
Juan Carlos Saez <jcsaezal@ucm.es>
2022-08-29 22:22:26 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+documents=('../Modulos/guion.md modulos')
+
+ndocs=${#documents[@]}
+
+## Clone image directory
+if [ -d img ]; then
+	rm -rf img
+fi
+
+cp -rf ../img .
+
+selected_document=$1
+
+if [ "${selected_document}" != "" ]; then
+	filter_on=1
+else
+	filter_on=0
+fi
+
+for (( i=0 ; $i<$ndocs ; i++ ))
+do
+	items=(${documents[$i]})
+	source_file=${items[0]}
+	target_file1=${items[1]}.html
+	target_file2=${items[1]}.pdf
+
+	if [ ${filter_on} -eq 1 ] && [ "${selected_document}" != "${items[1]}" ]; then
+		continue
+	fi
+
+	echo "${source_file} --> ${target_file1}"
+	pandoc ${source_file} -N --self-contained  --template pandoc-templates/idocs3.html-template.html --toc  -p -o ${target_file1} --highlight-style tango
+	echo "${source_file} --> ${target_file2}"
+	pandoc ${source_file} -N --toc --highlight-style tango -o ${target_file2}
+done
+
+
+rm -rf img
